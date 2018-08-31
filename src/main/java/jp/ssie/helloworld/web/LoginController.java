3d3bd90db14aff7b8f5dc65061ae8e3c984484e7
nissa-zhang <nissa.zhang@gmail.com>
2018-08-31 18:34:02 +0900

    step6 finished
    http://tech.pjin.jp/blog/2016/05/31/springframework7/

---
+++ src/main/java/jp/ssie/helloworld/web/LoginController.java
@@ -0,0 +1,32 @@
+package jp.ssie.helloworld.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.ssie.helloworld.form.LoginForm;
+import jp.ssie.helloworld.validation.GroupOrder;
+
+@Controller
+public class LoginController {
+@RequestMapping(value = "/", method = RequestMethod.GET)
+public String index(Model model) {
+return "index";
+}
+
+@RequestMapping(value = "/login", method = RequestMethod.POST)
+public String login(Model model, @Validated(GroupOrder.class) @ModelAttribute("loginForm") LoginForm loginForm, BindingResult result) {
+if(result.hasErrors()) {
+return "index";
+}
+
+model.addAttribute("loginName", loginForm.getLoginName());
+
+return "top";
+
+}
+}
